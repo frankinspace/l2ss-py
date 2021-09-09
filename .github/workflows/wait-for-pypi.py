fb8612d1bfb5fafcd8bf0e8b6c1ae115ea389d28
Frank Greguska <89428916+frankinspace@users.noreply.github.com>
2021-09-09 15:12:03 -0700

    workflows!

--- .github/workflows/wait-for-pypi.py
+++ .github/workflows/wait-for-pypi.py
@@ -6,6 +6,12 @@ import tempfile
 
 import tenacity
 
+'''
+Sometimes the package published to PyPi is not immediately available for download from the index. This script
+simply repeatedly tries to download a specific version of a package from PyPI (or test.pypi) until it succeeds or
+a limit is exceeded.
+'''
+
 
 @tenacity.retry(
     wait=tenacity.wait_exponential(multiplier=1, min=4, max=10),
@@ -16,7 +22,8 @@ import tenacity
 def download_package(package):
     subprocess.check_call([sys.executable, '-m',
                            'pip', '--isolated', '--no-cache-dir',
-                           'download', '--no-deps', '-d', tempfile.gettempdir(), '--index-url', 'https://pypi.org/simple/',
+                           'download', '--no-deps', '-d', tempfile.gettempdir(), '--index-url',
+                           'https://pypi.org/simple/',
                            '--extra-index-url', 'https://test.pypi.org/simple/', package
                            ])
 
